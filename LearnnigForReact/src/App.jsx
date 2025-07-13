bbafb102edd1377a9e229553a2e3dc42c9a641ab
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-13 20:23:43 +0700

    seting_component

--- LearnnigForReact/src/App.jsx
+++ LearnnigForReact/src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from "react";
 import Header from "./components/Header";
+import PersonList from "./components/PersonList";
 
 function App() {
-  const [data, setData] = useState([
-    {id: 1 ,name: "John" , age: 30 ,gender:"ชาย"},
-    {id: 2 ,name: "Jane" , age: 25 ,gender:"หญิง"},
-    {id: 3 ,name: "Doe" , age: 22 , gender:"ชาย"},
-    {id: 4 ,name: "Alice" , age: 28,gender:"หญิง"},
-  ]);
-  const [show, setShow] = useState(true)
-  console.table(data);
-
+ 
   return (
     <>
     <Header />
-    <div>
-      <h2>Data List</h2>
-      <h1>จำนวนประชากร : {data.length}</h1>
-      <button onClick={()=>setShow(!show)}>{show ? "ซ่อน" : "เเสดง"}</button>
-      <ul>
-        {show && data.map((item) =>(
-          <li key={item.id}>{item.name} | {item.age} | {item.gender}</li>
-        ))}
-      </ul>
-          
-    </div>
+    <PersonList />
     </>
   );
 }

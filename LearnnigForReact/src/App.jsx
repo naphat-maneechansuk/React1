a8b9b59cf24cf2c41aec2315060e1005f68d85b0
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-13 19:25:28 +0700

    เพิ่มลดอายุ

--- LearnnigForReact/src/App.jsx
+++ LearnnigForReact/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const name ="ณภัทร"
+  const [age, setAge] = useState(20);
+  function add(){
+    setAge(age + 1);
+  }
+  function subtract(){
+    setAge(age - 1);
+  }
   return (
+    <>
     <div>
-      <h1>Naphat</h1>
+      <h1>Naphat {name}</h1>
       <p>Welcome to my React application!</p>
+      <p>Age: {age}</p>
+      <button onClick={add}>เพิ่มอายุ</button>
+      <button onClick={subtract}>ลดอายุ</button>
     </div>
+    </>
   );
 }
 export default App;

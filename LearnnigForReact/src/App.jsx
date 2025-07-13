0d84e4008e4c0c35893f9da0377e04f404dac947
naphat-maneechansuk <141388294+naphat-maneechansuk@users.noreply.github.com>
2025-07-13 19:48:28 +0700

    showListData

--- LearnnigForReact/src/App.jsx
+++ LearnnigForReact/src/App.jsx
@@ -3,20 +3,35 @@ import { useState } from "react";
 function App() {
   const name ="ณภัทร"
   const [age, setAge] = useState(20);
-  function add(){
-    setAge(age + 1);
-  }
-  function subtract(){
-    setAge(age - 1);
-  }
+  const [data, setData] = useState([
+    { id: 1, name: "smith" ,gender:"ชาย"},
+    { id: 2, name: "saba" ,gender:"หญิง"},
+    { id: 3, name: "kalob" ,gender:"ชาย"},
+    { id: 4, name: "sara" ,gender:"หญิง"}
+  ]);
+  
+  const[show, setShow] = useState(true);
+
   return (
     <>
     <div>
       <h1>Naphat {name}</h1>
       <p>Welcome to my React application!</p>
       <p>Age: {age}</p>
-      <button onClick={add}>เพิ่มอายุ</button>
-      <button onClick={subtract}>ลดอายุ</button>
+      <button onClick={()=>setAge(age+1)}>เพิ่มอายุ</button>
+      <button onClick={()=>setAge(age-1)}>ลดอายุ</button>
+      <br />
+      <button onClick={()=>setAge(20)}>ล้าง</button>
+      <hr />
+      <h2>Data List</h2>
+      <h3>จำนวนประชากร {data.length}</h3>
+      <ul>
+        {data.map((item)=>(
+          <li key={item.id}>{item.name} | {item.gender}</li>
+        ))}
+        
+      </ul>
+
     </div>
     </>
   );
